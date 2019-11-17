8074aac849834139a33294b577cdb37cf3d949f5
brunorafael8 <bruninho_lito@hotmail.com>
2019-11-17 20:49:25 -0300

    (improve) App.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,64 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import styled from "styled-components";
 
-function App() {
+const Container = styled.div`
+  background-color: #e90052;
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const Content = styled.section`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  width: 10%;
+  height: 40%;
+`;
+
+const Title = styled.h1`
+  color: #38003c;
+  font-weight: bold;
+  padding: 5px;
+  font-size: 40px;
+  border-bottom-color: #38003c;
+  border-bottom-width: 3px;
+  margin-top: 40px;
+  letter-spacing: 6px;
+`;
+
+const SearchInput = styled.input`
+  width: 100%;
+  margin-top: 100px;
+  border-radius: 5px;
+  border: 1px;
+  padding: 15px;
+  border-color: #38003c;
+  background-color: white;
+`;
+
+const SearchButton = styled.button`
+  background-color: #38003c;
+  border-radius: 5px;
+  border: 0;
+  width: 100%;
+  margin-top: 15px;
+  padding: 15px;
+  color: #fff;
+`;
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Content>
+        <Title>GITUSER</Title>
+        <SearchInput placeholder="Ex: brunorafael8" />
+        <SearchButton>Search</SearchButton>
+      </Content>
+    </Container>
   );
-}
+};
 
 export default App;
